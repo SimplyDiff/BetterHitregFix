32e161e603379be8ea3d21560b27696d729c942e
PORTR\rober <robservil@alumn.us.es>
2026-05-06 12:30:16 +0200

    Add Lock Target keybind (N): set render target before attacking

--- src/main/java/you/jass/betterhitreg/BetterHitreg.java
+++ src/main/java/you/jass/betterhitreg/BetterHitreg.java
@@ -13,8 +13,11 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
+import you.jass.betterhitreg.hitreg.Hitreg;
 import you.jass.betterhitreg.settings.Commands;
 import you.jass.betterhitreg.ui.UIScreen;
 import you.jass.betterhitreg.utility.Render;
@@ -24,6 +27,7 @@ import static you.jass.betterhitreg.hitreg.Hitreg.*;
 public class BetterHitreg implements ModInitializer {
     public static KeyBinding uiKey;
     public static KeyBinding handKey;
+    public static KeyBinding lockTargetKey;
     public static int handSwitchCooldown;
 
     @Override
@@ -72,6 +76,11 @@ public class BetterHitreg implements ModInitializer {
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_I, category
         ));
+        lockTargetKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Lock Target",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_N, category
+        ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (uiKey.wasPressed() && client.currentScreen == null) client.setScreen(new UIScreen());
@@ -81,6 +90,19 @@ public class BetterHitreg implements ModInitializer {
                 client.player.setMainArm(client.options.getMainArm().getValue());
                 client.options.sendClientSettings();
                 handSwitchCooldown = 5;
+            }
+
+            while (lockTargetKey.wasPressed() && client.currentScreen == null) {
+                if (client.crosshairTarget instanceof EntityHitResult ehr
+                        && ehr.getEntity() instanceof LivingEntity living
+                        && living != client.player) {
+                    Hitreg.target = living;
+                    Hitreg.lastTarget = living.getId();
+                } else {
+                    // press again while not looking at anyone to clear manual target
+                    Hitreg.target = null;
+                    Hitreg.lastTarget = -1;
+                }
             }
 
             if (handSwitchCooldown > 0) handSwitchCooldown--;
